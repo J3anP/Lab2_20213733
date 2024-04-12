1a2824d4869d7d0f94427c4cec58bf7a61e76b53
J3anP <a20213733@pucp.edu.pe>
2024-04-11 21:34:57 -0500

    Lab2

---
+++ src/main/java/com/example/lab2gtics/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.lab2gtics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/buscaminas", method = RequestMethod.GET)
+    public String paginaPrincipal(){
+        return "configuracion";
+    }
+
+    @GetMapping("/jugar")
+    public String juego(){
+        return "juego";
+    }
+}
