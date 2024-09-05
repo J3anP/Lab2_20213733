3c3307126db028399a734ea42019e3eb90b5fc07
J3anP <jeanpiereipurre28@gmail.com>
2024-09-05 03:48:31 -0500

    diseño

---
+++ app/src/main/java/com/example/telegame/EstadisticasActivity.java
@@ -0,0 +1,13 @@
+package com.example.telegame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EstadisticasActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/example/telegame/MainActivity.java
+++ app/src/main/java/com/example/telegame/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.telegame;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +23,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        Log.d(TAG, "oncreate");
+        /*
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+         */
+        Button buttonStart = findViewById(R.id.buttonStartGame);
+        buttonStart.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            launcher.launch(intent);
+        });
     }
+
+    //Va al juego
+    public void irAlJuego(View view) {
+
+        Intent intent = new Intent(this, GameActivity.class);
+
+        startActivity(intent);
+
+    }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    String nombre = data.getStringExtra("nombre");
+                    Toast.makeText(MainActivity.this,
+                            "El nombre recibido es: " + nombre    ,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+    );
 }
