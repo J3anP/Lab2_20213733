6c6089bc20c94c52e1fed2b61695f4e466a9e5d1
J3anP <a20213733@pucp.edu.pe>
2024-09-08 00:48:54 -0500

    correcciones 3

--- app/src/main/java/com/example/telegame/EstadisticasActivity.java
+++ app/src/main/java/com/example/telegame/EstadisticasActivity.java
@@ -2,22 +2,23 @@ package com.example.telegame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class EstadisticasActivity extends AppCompatActivity {
-    //Bundle sirve para pasar información entre vistas (GOOGLEADO)
-    private Bundle intentExtras;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas);
 
-        intentExtras = getIntent().getExtras();
+        intent = getIntent();
         playerStatistics();
         btnNewNewGame();
     }
@@ -26,7 +27,7 @@ public class EstadisticasActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home){
             Intent resultIntent = new Intent();
-            resultIntent.putExtras(intentExtras);
+            resultIntent.putExtras(intent.getExtras());
             setResult(RESULT_OK, resultIntent);
             finish();
             return true;
@@ -36,23 +37,27 @@ public class EstadisticasActivity extends AppCompatActivity {
     }
 
     private void playerStatistics() {
-        String playerName = intentExtras.getString("name");
-        ((TextView) findViewById(R.id.name)).setText("Jugador: " + playerName);
+        String playerName = intent.getStringExtra("name");
+        TextView playerNameTV = findViewById(R.id.name);
+        playerNameTV.setText("Jugador: " + playerName);
 
-        TextView texVStatistics = findViewById(R.id.stats);
-        String statistics = intentExtras.getString("estadisticas");
+        TextView textVStatistics = findViewById(R.id.stats);
+        String playerStats = intent.getStringExtra("estadisticas");
 
-        if (!(statistics.isEmpty())) {
-            texVStatistics.setText(statistics);
+        if(!TextUtils.isEmpty(playerStats)){
+            textVStatistics.setText(playerStats);
+        }else{
+            textVStatistics.setText(R.string.no_stats_available);
         }
+
     }
 
     private void btnNewNewGame() {
         Button btnNewNewGame = findViewById(R.id.btnNewNewGame);
         btnNewNewGame.setOnClickListener(v -> {
             Intent resultIntent = new Intent();
-            resultIntent.putExtras(intentExtras);
-            resultIntent.putExtra("NewGame", "");
+            resultIntent.putExtras(intent.getExtras());
+            resultIntent.putExtra("NewGame", true);
             setResult(RESULT_OK, resultIntent);
             finish();
         });
